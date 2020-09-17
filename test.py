62d143465318dd9a7cdec281ee609d6db7fd6bf5
Tom E <tom.eichlersmith@gmail.com>
2020-09-17 13:12:41 -0500

    updates to continuous integration and testing

---
+++ test.py
@@ -0,0 +1,25 @@
+
+import numpy
+
+import ROOT
+from ROOT import TTree
+from ROOT import TBranch
+
+import uproot
+
+import matplotlib
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import pyplot
+
+import xgboost
+
+import sklearn
+from sklearn import datasets
+from sklearn.datasets import make_blobs
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import LinearSVC
+from sklearn.calibration import calibration_curve
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
