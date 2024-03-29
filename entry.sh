60e7f618e03ef0b70158763a0291b426bd792d4d
tomeichlersmith <eichl008@umn.edu>
2024-03-29 08:39:38 -0500

    forgot to rename env-init.sh script in entry.sh

--- entry.sh
+++ entry.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-source /etc/env-init.sh
+source /etc/ldmx-env-init.sh
 
 # puts a config/cache directory for matplotlib to use
-# does /not/ need to be in the env-init.sh script since denv-based
+# does /not/ need to be in the ldmx-env-init.sh script since denv-based
 # interactions with the image define LDMX_BASE as HOME for us anyways
 export MPLCONFIGDIR=$LDMX_BASE/.config/matplotlib
 
